8368f2bb0ae5035f70f96afe087153b884969fe1
fzt <1427068419@qq.com>
2023-05-30 08:06:39 +0800

    完成向服务器获取 guessCity

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -3,20 +3,48 @@
         <head-top signin-up="home">
             <span slot="logo" class="head_logo" @click="reload">ele.me</span>
         </head-top>
-        
+        <nav class="city_nav">
+            <div class="city_tip">
+                <span>当前定位城市：</span>
+                <span>定位不准时，请在城市列表中选择</span>
+            </div>
+            <router-link :to="'/city/' + guessCityid" class="guess_city">
+                <span>{{ guessCity }}</span>
+                <svg class="arrow_right">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+                </svg>
+            </router-link>
+        </nav>
     </div>
 </template>
 
 <script>
 import headTop from '../../components/header/head'
+import {cityGuess} from '../../service/getData'
 
 export default {
     data() {
-        return {}
+        return {
+            guessCity: '',      //当前城市
+            guessCityid: ''     //当前城市id
+        }
+    },
+
+    mounted() {
+        cityGuess().then(res => {
+            this.guessCity = res.name;
+            this.guessCityid = res.id;
+        })
     },
 
     components: {
         headTop
+    },
+    methods: {
+        //点击图标刷新页面
+        reload() {
+            window.location.reload();
+        }
     }
 }
 </script>
@@ -32,4 +60,39 @@ export default {
     @include ct;
 }
 
+.city_nav {
+    padding-top: 2.35rem;
+    border-top: 1px solid $bc;
+    background-color: #fff;
+    margin-bottom: 0.4rem;
+    .city_tip{
+        @include fj;
+        line-height: 1.45rem;
+        padding: 0 0.45rem;
+        span:nth-of-type(1){
+            @include sc(0.55rem, #666);
+        }
+        span:nth-of-type(2){
+            font-weight: 900;
+            @include sc(0.475rem, #9f9f9f);
+        }
+
+    }
+    .guess_city{
+        @include fj;
+        align-items: center;
+        height: 1.8rem;
+        padding: 0 0.45rem;
+        border-top: 1px solid $bc;
+        border-bottom: 2px solid $bc;
+        @include font(0.75rem, 1.8rem);
+        span:nth-of-type(1){
+            color: $blue;
+        }
+        .arrow_right{
+            @include wh(.6rem, .6rem);
+            fill: #999;
+        }
+    }
+}
 </style>
