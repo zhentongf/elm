4b8041c9ea3ba11c3e784f125f52a0630279f304
fzt <1427068419@qq.com>
2023-06-12 12:19:54 +0800

    跨越Set-Cookie被block导致验证码失效

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -75,7 +75,7 @@
             },
             // 获取验证码
             async getCaptchaCode(){
-                // 必须用https获取验证码。用http获取，验证码会立即失效
+                // 如果发给后端cookie不正确，导致验证码失效
                 let res = await getcaptchas();
                 this.captchaCodeImg = res.code;
             },
